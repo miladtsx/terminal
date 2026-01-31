f217411f99adec0cb5497f1d6ecbde63f8b1c6ce
tsx <miladtsx@gmail.com>
2026-01-31 10:11:53 +0330

    fix(copy): title

--- src/hooks/useTerminalController.ts
+++ src/hooks/useTerminalController.ts
@@ -348,7 +348,7 @@ export function useTerminalController(props: TerminalProps): ControllerReturn {
         startLines[0] = [
           buildAvatarSegment([startLines[0]], {
             label: "Milad",
-            meta: "Backend Engineer",
+            meta: "Senior Backend & Full Stack Engineer",
           }),
         ];
       }
